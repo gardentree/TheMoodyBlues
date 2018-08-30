2cab36776aa15a55a4618abe87870d530b8cffd3
(ю:】 <gardentreex@gmail.com>
2018-08-30 19:26:44 +0900

    :+1: Improve retweeter

--- src/renderer/Timeline.tsx
+++ src/renderer/Timeline.tsx
@@ -66,7 +66,7 @@ export class Timeline extends React.Component<Property,Property> {
                   {medias && <div>{medias}</div>}
                 </div>
                 {quote && <div className="quote"><div>@{quote.user.screen_name}</div><p><PrettyTweet tweet={quote} /></p></div>}
-                {retweet && <div>{retweet.user.screen_name} retweeted</div>}
+                {retweet && <div className='retweeter'>{retweet.user.screen_name} retweeted</div>}
               </div>
             </div>
           </li>
