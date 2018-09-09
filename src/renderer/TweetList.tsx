665d5e40e443abfac627d2437bf483ee5cb8f4a7
(ю:】 <gardentreex@gmail.com>
2018-09-09 14:51:23 +0900

    :shower: Remove animation

--- src/renderer/TweetList.tsx
+++ src/renderer/TweetList.tsx
@@ -1,6 +1,5 @@
 import {remote,shell} from 'electron'
 import * as React from "react";
-import {CSSTransition,TransitionGroup} from "react-transition-group";
 import {PrettyTweet} from "./PrettyTweet";
 import {MediaBox} from "./MediaBox";
 import {Tweet,Media} from "./twitter";
@@ -52,40 +51,32 @@ export class TweetList extends React.Component<Property,{}> {
       }
 
       return (
-        <CSSTransition
-          key={tweet_status.id_str}
-          classNames="fade"
-          timeout={300}
-        >
-          <li key={tweet_status.id_str} data-url={`https://twitter.com/${tweet_status.user.screen_name}/status/${tweet_status.id_str}`} onContextMenu={this.openContextMenu}>
-            <div>
-              <div className={`avatar${retweet ? ' retweet':''}`}>
-                <img src={tweet.user.profile_image_url_https} className="tweeter" />
-                {retweet && <img src={retweet.user.profile_image_url_https} className="retweeter" />}
-              </div>
-              <div className='tweet'>
-                <div>
-                  <div className='meta'>
-                    <div className='screen_name'>@{tweet.user.screen_name}</div>
-                    <div className='created_at'>{TweetList.prettyTime(tweet.created_at)}</div>
-                  </div>
-                  <p><PrettyTweet tweet={tweet} /></p>
-                  {medias && <MediaBox medias={medias} />}
-                </div>
-                {quote && <div className="quote"><div className='screen_name'>@{quote.user.screen_name}</div><p><PrettyTweet tweet={quote} /></p></div>}
-                {retweet && <div className='retweeter'>Retweeted by {retweet.user.screen_name}</div>}
-              </div>
+        <li key={tweet_status.id_str} data-url={`https://twitter.com/${tweet_status.user.screen_name}/status/${tweet_status.id_str}`} onContextMenu={this.openContextMenu}>
+          <div>
+            <div className={`avatar${retweet ? ' retweet':''}`}>
+              <img src={tweet.user.profile_image_url_https} className="tweeter" />
+              {retweet && <img src={retweet.user.profile_image_url_https} className="retweeter" />}
             </div>
-          </li>
-        </CSSTransition>
+            <div className='tweet'>
+              <div>
+                <div className='meta'>
+                  <div className='screen_name'>@{tweet.user.screen_name}</div>
+                  <div className='created_at'>{TweetList.prettyTime(tweet.created_at)}</div>
+                </div>
+                <p><PrettyTweet tweet={tweet} /></p>
+                {medias && <MediaBox medias={medias} />}
+              </div>
+              {quote && <div className="quote"><div className='screen_name'>@{quote.user.screen_name}</div><p><PrettyTweet tweet={quote} /></p></div>}
+              {retweet && <div className='retweeter'>Retweeted by {retweet.user.screen_name}</div>}
+            </div>
+          </div>
+        </li>
       )
     });
 
     return (
       <ul className="timeline">
-        <TransitionGroup>
-          { items }
-        </TransitionGroup>
+        { items }
       </ul>
     );
   }
