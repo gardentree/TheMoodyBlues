719409ce3247ab75d63f0967e610c4ce8dab8128
(ю:】 <gardentreex@gmail.com>
2018-09-10 01:34:17 +0900

    :+1: Make to focus tweet

--- src/renderer/TweetList.tsx
+++ src/renderer/TweetList.tsx
@@ -6,15 +6,23 @@ interface Property {
   tweets: twitter.Tweet[];
 }
 
-export class TweetList extends React.Component<Property,{}> {
+export class TweetList extends React.Component<Property,{focus: string|null}> {
   constructor(property: Property) {
     super(property);
+
+    this.state = {focus: null}
+
+    this.setFocus = this.setFocus.bind(this);
+  }
+
+  setFocus(event: React.SyntheticEvent<HTMLElement>) {
+    this.setState({focus: event.currentTarget.dataset.id!});
   }
 
   render() {
     const elements = this.props.tweets.map((tweet) => {
       return (
-        <li key={tweet.id_str}><Tweet source={tweet} /></li>
+        <li key={tweet.id_str} data-id={tweet.id_str} className={this.state.focus == tweet.id_str ? 'focus':''} onClick={this.setFocus}><Tweet source={tweet} /></li>
       )
     });
 
