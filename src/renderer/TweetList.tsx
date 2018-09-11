91448cc677b1e0e2984d35892939509d9eb94322
(ю:】 <gardentreex@gmail.com>
2018-09-11 19:33:11 +0900

    :recycle: Refactor tweet focus

--- src/renderer/TweetList.tsx
+++ src/renderer/TweetList.tsx
@@ -6,19 +6,15 @@ interface Property {
   tweets: twitter.Tweet[];
 }
 
-export class TweetList extends React.Component<Property,{focus: string|null,latest: number}> {
+export class TweetList extends React.Component<Property,{latest: number}> {
   constructor(property: Property) {
     super(property);
 
-    this.state = {focus: null,latest: 0}
+    this.state = {latest: 0}
 
-    this.setFocus = this.setFocus.bind(this);
     this.scrolling = this.scrolling.bind(this);
   }
 
-  setFocus(event: React.SyntheticEvent<HTMLElement>) {
-    this.setState({focus: event.currentTarget.dataset.id!});
-  }
   scrolling(event: React.SyntheticEvent<HTMLElement>): void {
     if ((event.target as HTMLElement).scrollTop <= 0 && this.props.tweets.length > 0 && this.state.latest < this.props.tweets[0].id) {
       this.setState({latest: this.props.tweets[0].id});
@@ -28,7 +24,7 @@ export class TweetList extends React.Component<Property,{focus: string|null,late
   render() {
     const elements = this.props.tweets.map((tweet) => {
       return (
-        <li key={tweet.id_str} data-id={tweet.id_str} className={this.state.focus == tweet.id_str ? 'focus':''} onClick={this.setFocus}>
+        <li key={tweet.id_str} data-id={tweet.id_str} tabIndex={-1}>
           <Tweet source={tweet} unread={this.state.latest > 0 && tweet.id > this.state.latest} />
         </li>
       )
