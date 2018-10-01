d70228c05b95bcb1c9ccdcbaa6daa39c0b758f90
(ю:】 <gardentreex@gmail.com>
2018-10-01 19:22:56 +0900

    :+1: Improve test launcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
 var gulp = require("gulp");
 var exec = require("child_process").exec;
+var fs = require("fs");
 
-gulp.task("test", function() {
-  exec("yarn test", function(error, stdout, stderr) {
+function execute(command) {
+  exec(command, function(error, stdout, stderr) {
     console.log(stdout);
 
     console.error(stderr);
   });
-});
+}
 
-gulp.task("default", function() {
-  gulp.watch(["src/**/*.ts?", "test/**/*.ts?"], function(event) {
-    exec(`NODE_ENV=test TS_NODE_PROJECT=tsconfig.test.json ./node_modules/.bin/electron-mocha --renderer --require ts-node/register '${event.path}' --require ignore-styles`, function(error, stdout, stderr) {
-      console.log(stdout);
+gulp.task("test", function() {
+  execute("yarn test:all");
 
-      console.error(stderr);
-    });
+  gulp.watch(["src/**/*.{ts,tsx}", "test/**/*_test.{ts,tsx}"], function(event) {
+    console.log(`${event.type}: ${event.path}`);
+
+    var relative = event.path.slice(process.cwd().length);
+
+    var command;
+    if (relative.match(/^\/test/)) {
+      command = `yarn test '${event.path}'`;
+    } else {
+      var matcher = relative.match(/^\/src\/(.+)\.(ts|tsx)$/);
+      if (matcher) {
+        var test = `${process.cwd()}/test/${matcher[1]}_test.${matcher[2]}`;
+        if (fs.existsSync(test)) {
+          command = `yarn test '${test}'`;
+        } else {
+          command = "yarn test:all";
+        }
+      } else {
+        command = "yarn test:all";
+      }
+    }
+
+    execute(command);
   });
 });
