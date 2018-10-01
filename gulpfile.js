ed76611afce075b1b8cd75fb64cfb6e6ddfede11
(ю:】 <gardentreex@gmail.com>
2018-10-01 17:32:14 +0900

    :green_heart: Improve test watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 var gulp = require("gulp");
 var exec = require("child_process").exec;
 
-gulp.task("test",function() {
-  exec("yarn test",function(error,stdout,stderr) {
+gulp.task("test", function() {
+  exec("yarn test", function(error, stdout, stderr) {
     console.log(stdout);
 
     console.error(stderr);
   });
 });
 
-gulp.task("default",function() {
-  gulp
-    .watch(["src/**","test/**"],["test"]);
+gulp.task("default", function() {
+  gulp.watch(["src/**/*.ts?", "test/**/*.ts?"], function(event) {
+    exec(`NODE_ENV=test TS_NODE_PROJECT=tsconfig.test.json ./node_modules/.bin/electron-mocha --renderer --require ts-node/register '${event.path}' --require ignore-styles`, function(error, stdout, stderr) {
+      console.log(stdout);
+
+      console.error(stderr);
+    });
+  });
 });
