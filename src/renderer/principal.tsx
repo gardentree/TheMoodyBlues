3967615914ec02492eebcc6096af245b6607b13e
(ю:】 <gardentreex@gmail.com>
2018-10-03 20:55:02 +0900

    :recycle: Refactor renderer index

---
+++ src/renderer/principal.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./modules/reducer";
+import rootSaga from "./sagas";
+import {Provider} from "react-redux";
+import Principal from "./components/Principal";
+import keybinds from "./helpers/keybinds";
+import authorize from "./helpers/authentication";
+
+import "photon/dist/css/photon.css";
+import "./index.scss";
+
+export default function launch() {
+  (async () => {
+    const client = await new Promise((resolve, reject) => {
+      resolve(authorize());
+    });
+
+    setup(client);
+  })();
+}
+
+function setup(twitter: any) {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+  sagaMiddleware.run(rootSaga);
+
+  keybinds(store);
+  store.getState()["account"] = twitter;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <Principal />
+    </Provider>,
+    document.getElementById("app")
+  );
+}
