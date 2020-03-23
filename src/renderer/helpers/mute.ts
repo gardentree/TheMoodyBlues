5ad06688dd2057c9f140b6c395f5b8e2b2d55752
(ю:】 <gardentreex@gmail.com>
2020-03-23 20:52:53 +0900

    :beetle: Fix mute when url is upper case

--- src/renderer/helpers/mute.ts
+++ src/renderer/helpers/mute.ts
@@ -6,7 +6,7 @@ const keywords = preferences.getMuteKeywords();
 export default function mute(tweets: TweetType[]): TweetType[] {
   return tweets.filter((tweet) => {
     if (!keywords.every((keyword) => tweet.full_text.toLowerCase().indexOf(keyword) < 0)) return false;
-    if (!keywords.every((keyword) => tweet.entities.urls.every((entity) => entity.display_url.indexOf(keyword) < 0))) return false;
+    if (!keywords.every((keyword) => tweet.entities.urls.every((entity) => entity.display_url.toLowerCase().indexOf(keyword) < 0))) return false;
 
     return true;
   });
