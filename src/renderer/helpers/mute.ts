6971cad4af6357e6cd676015840819b84ff8633a
(ю:】 <gardentreex@gmail.com>
2021-11-27 01:17:15 +0900

    :sparkles: Mute self retweets

--- src/renderer/helpers/mute.ts
+++ src/renderer/helpers/mute.ts
@@ -7,6 +7,12 @@ export default function mute(tweets: Twitter.Tweet[]): Twitter.Tweet[] {
     if (!keywords.every((keyword) => tweet.full_text.toLowerCase().indexOf(keyword) < 0)) return false;
     if (!keywords.every((keyword) => tweet.entities.urls.every((entity) => entity.display_url.toLowerCase().indexOf(keyword) < 0))) return false;
 
+    if (tweet.retweeted_status) {
+      if (tweet.user.id_str == tweet.retweeted_status.user.id_str) {
+        return false;
+      }
+    }
+
     return true;
   });
 }
