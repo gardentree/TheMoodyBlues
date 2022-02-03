060ca215927afb96111d8249b49e70cdba1d4f2f
(ю:】 <gardentreex@gmail.com>
2022-02-03 15:34:23 +0900

    :recycle: Use hooks

--- src/renderer/preferences/components/Preferences/component.tsx
+++ src/renderer/preferences/components/Preferences/component.tsx
@@ -1,49 +1,44 @@
-import React, {Suspense} from "react";
+import React, {Suspense, useState} from "react";
 import Timelines from "../Timelines";
 import Mute from "../Mute";
 
 const components: {[key: string]: React.FC} = {Timelines, Mute};
 
-class Preferences extends React.Component<{}, {active: string}> {
-  state = {active: "Timelines"};
+const Preferences = () => {
+  const [active, setActive] = useState("Timelines");
 
-  render() {
-    const {active} = this.state;
+  return (
+    <div className="Preferences">
+      <div className="window">
+        <header className="toolbar toolbar-header">
+          <h1 className="title">Preferences</h1>
+        </header>
 
-    return (
-      <div className="Preferences">
-        <div className="window">
-          <header className="toolbar toolbar-header">
-            <h1 className="title">Preferences</h1>
-          </header>
-
-          <div className="tab-group">
-            {Object.entries(components).map(([title, component]) => {
-              return (
-                <div
-                  key={title}
-                  className={`tab-item${active == title ? " active" : ""}`}
-                  onClick={() => {
-                    this.setState({active: title});
-                  }}
-                >
-                  {title}
-                </div>
-              );
-            })}
-          </div>
-
+        <div className="tab-group">
           {Object.entries(components).map(([title, component]) => {
             return (
-              <div key={title} className="window-content" style={{display: active == title ? "block" : "none"}}>
-                <Suspense fallback={<p>Loading...</p>}>{React.createElement(component)}</Suspense>
+              <div
+                key={title}
+                className={`tab-item${active == title ? " active" : ""}`}
+                onClick={() => {
+                  setActive(title);
+                }}
+              >
+                {title}
               </div>
             );
           })}
         </div>
-      </div>
-    );
-  }
-}
 
+        {Object.entries(components).map(([title, component]) => {
+          return (
+            <div key={title} className="window-content" style={{display: active == title ? "block" : "none"}}>
+              <Suspense fallback={<p>Loading...</p>}>{React.createElement(component)}</Suspense>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default Preferences;
