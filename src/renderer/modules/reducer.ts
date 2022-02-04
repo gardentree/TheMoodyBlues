87308678b3fdc917410432b95bb2f407fca9f34c
(ю:】 <gardentreex@gmail.com>
2022-02-04 22:11:30 +0900

    :shower: Clean

--- src/renderer/modules/reducer.ts
+++ src/renderer/modules/reducer.ts
@@ -1,13 +1,11 @@
 import {combineReducers} from "redux";
 import {reducer as formReducer} from "redux-form";
 
-import agent from "./agent";
 import timelines from "./timelines";
 import subcontents from "./subcontents";
 import principal from "./principal";
 
 export default combineReducers({
-  agent,
   timelines,
   subcontents,
   principal,
