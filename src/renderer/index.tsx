b0f863fc376f1f330beb50b6d4b2e726c36afbb3
(ю:】 <gardentreex@gmail.com>
2022-02-23 16:04:29 +0900

    :recycle: Merge principal.tsx into index.tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,18 @@
-import launchPrinciapl from "./principal";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import {createLogger} from "redux-logger";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fab} from "@fortawesome/free-brands-svg-icons";
 import {faSpinner} from "@fortawesome/free-solid-svg-icons";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import rootSaga from "./sagas";
+import rootReducer from "./modules/reducer";
+import * as actions from "@actions";
+import {setup as setupEvents} from "./events";
 import VerifierForm from "./components/VerifierForm";
+import Principal from "./components/Principal";
 
 (() => {
   require("photon/dist/css/photon.css");
@@ -25,7 +33,20 @@ facade.events.onShowVerifierForm(() => {
   ReactDOM.render(<VerifierForm callback={callback} />, document.getElementById("app"));
 });
 facade.events.onLaunch(() => {
-  launchPrinciapl();
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, createLogger()));
+
+  sagaMiddleware.run(rootSaga);
+
+  setupEvents(store);
+  store.dispatch(actions.prepareState());
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <Principal />
+    </Provider>,
+    document.getElementById("app")
+  );
 });
 facade.events.onAlert((error: unknown) => {
   if (error instanceof Error) {
