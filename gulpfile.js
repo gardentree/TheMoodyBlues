96f9aa51087d636e286380f26e54ad616ceb0528
(ю:】 <gardentreex@gmail.com>
2022-03-10 10:45:20 +0900

    :green_heart: Improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,19 @@
-var gulp = require("gulp");
-var exec = require("child_process").exec;
-var fs = require("fs");
+const gulp = require("gulp");
+const mocha = require("gulp-mocha");
+const typescript = require("ts-node/register");
 
-function execute(command) {
-  exec(command, function (error, stdout, stderr) {
-    console.log(stdout);
+gulp.task("test:all", () => {
+  process.env.NODE_ENV = "test";
+  process.env.TS_NODE_PROJECT = "tsconfig/test.json";
 
-    if (error) {
-      console.error(stderr);
-    }
-  });
-}
+  return gulp.src("test/**/*_test.ts", {read: false}).pipe(
+    mocha({
+      require: ["ts-node/register/transpile-only", "tsconfig-paths/register", "test/helper.ts"],
+      reporter: "nyan",
+    })
+  );
+});
 
-gulp.task("test", function () {
-  execute("yarn test:all");
-
-  const watcher = gulp.watch(["src/**/*.{ts,tsx}", "test/**/*_test.{ts,tsx}"]);
-  watcher.on("change", function (path) {
-    execute("clear");
-    console.log(`changed ${path}`);
-
-    var command;
-    if (path.match(/^test/)) {
-      command = `yarn test '${path}'`;
-    } else {
-      var matcher = path.match(/^src\/(.+)\.(ts|tsx)$/);
-      if (matcher) {
-        var test = `test/${matcher[1]}_test.${matcher[2]}`;
-        if (fs.existsSync(test)) {
-          command = `yarn test '${test}'`;
-        } else {
-          command = "yarn test:all";
-        }
-      } else {
-        command = "yarn test:all";
-      }
-    }
-
-    execute(command);
-  });
+gulp.task("watch", () => {
+  return gulp.watch(["src/**", "test/**"], gulp.series("test:all"));
 });
