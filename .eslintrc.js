0b8f3f6c17505d62db878d0bdd29f239a3e4a7c8
(ю:】 <gardentreex@gmail.com>
2022-03-11 16:17:37 +0900

    :shirt: Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-empty-function": ["off"],
+    "@typescript-eslint/no-non-null-assertion": ["off"],
+    "@typescript-eslint/no-unused-vars": ["off"],
+    "linebreak-style": ["error", "unix"],
+    "no-useless-escape": ["off"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
