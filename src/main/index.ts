6e777a72aae234323ea2252469dffdb0dc4353fb
(ю:】 <gardentreex@gmail.com>
2022-03-14 11:20:41 +0900

    :shower: Clean

--- src/main/index.ts
+++ src/main/index.ts
@@ -64,13 +64,6 @@ function createMainWindow() {
   return window;
 }
 
-app.on("activate", () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow();
-  }
-});
-
 // create main BrowserWindow when electron is ready
 app.on("ready", () => {
   mainWindow = createMainWindow();
