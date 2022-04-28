25fd4280fa2edc10ae18772a89ce17ef6ba08411
(ю:】 <gardentreex@gmail.com>
2022-04-28 15:25:47 +0900

    :+1: Wrap window actions

--- src/renderer/sagas/window.ts
+++ src/renderer/sagas/window.ts
@@ -1,46 +1,47 @@
 import {takeLatest, takeEvery} from "redux-saga/effects";
 import * as actions from "@actions";
 import {Action, BaseAction} from "redux-actions";
+import {wrap} from "./library";
 
-function focusTweet(action: Action<{tweet: Twitter.Tweet}>) {
+function* focusTweet(action: Action<{tweet: Twitter.Tweet}>) {
   const {tweet} = action.payload;
   const container = getContainer();
   const element = <HTMLElement>container.querySelector(`li[data-id="${tweet.id_str}"] *[tabindex='-1']`)!;
 
-  scrollTo(container, element, 500);
-  element.focus();
+  yield scrollTo(container, element, 500);
+  yield element.focus();
 }
 
-function focusLatestTweet(action: BaseAction) {
+function* focusLatestTweet(action: BaseAction) {
   const container = getContainer();
   const latest = <HTMLElement>container.querySelector("li:first-child *[tabindex='-1']")!;
 
-  scrollTo(container, latest, 500);
-  latest.focus();
+  yield scrollTo(container, latest, 500);
+  yield latest.focus();
 }
 
-function focusUnreadTweet(action: BaseAction) {
+function* focusUnreadTweet(action: BaseAction) {
   const container = getContainer();
   const unreads = container.querySelectorAll("li.unread *[tabindex='-1']");
   const oldest = <HTMLElement>Array.from(unreads).slice(-1)[0];
 
   if (oldest) {
-    scrollTo(container, oldest, 500);
-    oldest.focus();
+    yield scrollTo(container, oldest, 500);
+    yield oldest.focus();
   } else {
-    focusLatestTweet(action);
+    yield focusLatestTweet(action);
   }
 }
 
-function alarm(action: Action<{message: string}>) {
-  window.alert(action.payload.message);
+function* alarm(action: Action<{message: string}>) {
+  yield window.alert(action.payload.message);
 }
 
 // prettier-ignore
 export default [
-  takeLatest(actions.focusTweet.toString(), focusTweet),
-  takeLatest(actions.focusLatestTweet.toString(), focusLatestTweet),
-  takeLatest(actions.focusUnreadTweet.toString(), focusUnreadTweet),
+  takeLatest(actions.focusTweet.toString(), wrap(focusTweet)),
+  takeLatest(actions.focusLatestTweet.toString(), wrap(focusLatestTweet)),
+  takeLatest(actions.focusUnreadTweet.toString(), wrap(focusUnreadTweet)),
   takeEvery(actions.alarm.toString(), alarm),
 ];
 
