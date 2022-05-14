82763981d8ebfe8f850a1f75b2862694b39a637c
(ю:】 <gardentreex@gmail.com>
2022-05-14 16:40:11 +0900

    :recycle: Use react-error-boundary

--- src/renderer/components/TweetList/component.tsx
+++ src/renderer/components/TweetList/component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Tweet from "../Tweet";
-import {default as TweetErrorBoundary} from "../Tweet/ErrorBoundary";
+import {ErrorBoundary} from "react-error-boundary";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 export interface OwnProps {
@@ -10,16 +10,32 @@ export interface OwnProps {
 }
 type Props = OwnProps;
 
+const {facade} = window;
+
 const TweetList = (props: Props) => {
   const {tweets, lastReadID} = props;
+
   const elements = tweets.map((tweet) => {
     const unread = lastReadID != null && tweet.id_str > lastReadID;
+
+    const errorHandler = (error: Error, information: {componentStack: string}) => {
+      facade.logger.error(error.stack);
+      facade.logger.error(tweet);
+    };
+
     return (
       <CSSTransition key={tweet.id_str} timeout={500} classNames="fade">
         <li data-id={tweet.id_str} className={unread ? "unread" : undefined}>
-          <TweetErrorBoundary tweet={tweet}>
+          <ErrorBoundary
+            onError={errorHandler}
+            fallbackRender={({error, resetErrorBoundary}) => (
+              <div>
+                {tweet.id_str}: {error.message}
+              </div>
+            )}
+          >
             <Tweet source={tweet} unread={unread} />
-          </TweetErrorBoundary>
+          </ErrorBoundary>
         </li>
       </CSSTransition>
     );
