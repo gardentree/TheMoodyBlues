ff8c56b0aa10df1d452d36d03f92351dd3949ca7
(ю:】 <gardentreex@gmail.com>
2022-10-21 00:17:31 +0900

    :green_heart: Fix test/renderer/sagas/library_test.ts

---
+++ test/renderer/sagas/library_test.ts
@@ -0,0 +1,28 @@
+import {expectSaga} from "redux-saga-test-plan";
+import {wrap} from "@source/renderer/sagas/library";
+
+describe(wrap.name, () => {
+  it("hide loading spinner when alarm", () => {
+    const error = new Error();
+    function* fail() {
+      throw error;
+
+      yield;
+    }
+
+    return expectSaga(wrap(fail), {type: "", payload: undefined, meta: undefined})
+      .put({
+        type: "principal/showLoading",
+        payload: true,
+      })
+      .put({
+        type: "alarm",
+        payload: {message: error},
+      })
+      .put({
+        type: "principal/showLoading",
+        payload: false,
+      })
+      .run();
+  });
+});
