f7f6a99923eb6b62a25550eca11846da48fa2732
(ю:】 <gardentreex@gmail.com>
2022-10-30 18:49:21 +0900

    :sparkles: Make to delete taboo in preference

--- src/renderer/preferences/components/Gatekeeper/component.tsx
+++ src/renderer/preferences/components/Gatekeeper/component.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {useState, useEffect} from "react";
 import {GATEKEEPER, EVERYONE} from "@shared/defaults";
 import * as DateUtility from "date-fns";
+import lodash from "lodash";
 
 const {facade} = window;
 
@@ -36,11 +37,28 @@ const Gatekeeper = () => {
       );
     });
 
+  function handleDelete(keyword: string) {
+    const newGatekeeper = lodash.cloneDeep(gatekeeper);
+    const newPassenger = newGatekeeper.passengers[currentPassenger.identifier];
+    delete newPassenger.taboos[keyword];
+
+    facade.storage.setGatekeeperPreference(newGatekeeper);
+    setGatekeeper(newGatekeeper);
+    setCurrentPassenger(newPassenger);
+  }
+
   const tabooRows = Object.values(currentPassenger.taboos).map((taboo) => {
     return (
       <tr key={taboo.keyword}>
         <td>{taboo.keyword}</td>
         <td>{taboo.expireAt ? `${DateUtility.format(new Date(taboo.expireAt), "yyyy-MM-dd HH:mm")}まで` : "無期限"}</td>
+        <td
+          onClick={() => {
+            handleDelete(taboo.keyword);
+          }}
+        >
+          <span className="icon icon-cancel"></span>
+        </td>
       </tr>
     );
   });
@@ -53,9 +71,10 @@ const Gatekeeper = () => {
       <div className="pane">
         <table className="table-striped">
           <thead>
-            <tr>
+            <tr className="active">
               <th>Keyword</th>
-              <th>Expire At</th>
+              <th style={{width: "180px"}}>Expire At</th>
+              <th style={{width: "40px"}}></th>
             </tr>
           </thead>
           <tbody>{tabooRows}</tbody>
