3d46fc708d645d57b3da26c4ac6948f8bca7fa8e
(ю:】 <gardentreex@gmail.com>
2022-10-31 22:09:21 +0900

    :rocket: Use useMemo

--- src/renderer/components/Tweet.tsx
+++ src/renderer/components/Tweet.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from "react";
 import UserIdentifier from "./Tweet/UserIdentifier";
 import TweetBody from "./Tweet/TweetBody";
 import MediaBox from "./Tweet/MediaBox";
@@ -13,49 +14,53 @@ interface OwnProps {
 const Tweet = (props: OwnProps) => {
   const {source, unread} = props;
 
-  let tweet: Twitter.Tweet, retweet: Twitter.Tweet | null;
-  if (source.retweeted_status === undefined) {
-    tweet = source;
-    retweet = null;
-  } else {
-    tweet = source.retweeted_status!;
-    retweet = source;
-  }
+  const memo = useMemo(() => {
+    let tweet: Twitter.Tweet, retweet: Twitter.Tweet | null;
+    if (source.retweeted_status === undefined) {
+      tweet = source;
+      retweet = null;
+    } else {
+      tweet = source.retweeted_status!;
+      retweet = source;
+    }
 
-  let quote: Twitter.Tweet | null = null;
-  if (tweet.quoted_status !== undefined) {
-    quote = tweet.quoted_status;
-  }
+    let quote: Twitter.Tweet | null = null;
+    if (tweet.quoted_status !== undefined) {
+      quote = tweet.quoted_status;
+    }
 
-  let media: Twitter.Media[] = [];
-  if (tweet.extended_entities !== undefined) {
-    media = tweet.extended_entities.media;
-  }
+    let media: Twitter.Media[] = [];
+    if (tweet.extended_entities !== undefined) {
+      media = tweet.extended_entities.media;
+    }
 
-  return (
-    <div data-url={`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`} onContextMenu={showMenuForTweet(source)} tabIndex={-1}>
-      <div className={`avatar${retweet ? " retweet" : ""}${unread ? " unread" : ""}`}>
-        <img src={tweet.user.profile_image_url_https} className="tweeter" />
-        {retweet && <img src={retweet.user.profile_image_url_https} className="retweeter" />}
-      </div>
-      <div className="tweet">
-        <div>
-          <div className="meta">
-            <div className="screen_name">
-              <UserIdentifier identifier={tweet.user.screen_name} />
-            </div>
-            <div className="created_at">{prettyTime(tweet.created_at)}</div>
-          </div>
-          <p>
-            <TweetBody tweet={tweet} expand={true} />
-          </p>
-          {media && <MediaBox media={media} />}
+    return (
+      <div data-url={`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`} onContextMenu={showMenuForTweet(source)} tabIndex={-1}>
+        <div className={`avatar${retweet ? " retweet" : ""}${unread ? " unread" : ""}`}>
+          <img src={tweet.user.profile_image_url_https} className="tweeter" />
+          {retweet && <img src={retweet.user.profile_image_url_https} className="retweeter" />}
         </div>
-        {quote && <Quotation tweet={quote} />}
-        {retweet && <div className="retweeter">Retweeted by {retweet.user.screen_name}</div>}
+        <div className="tweet">
+          <div>
+            <div className="meta">
+              <div className="screen_name">
+                <UserIdentifier identifier={tweet.user.screen_name} />
+              </div>
+              <div className="created_at">{prettyTime(tweet.created_at)}</div>
+            </div>
+            <p>
+              <TweetBody tweet={tweet} expand={true} />
+            </p>
+            {media && <MediaBox media={media} />}
+          </div>
+          {quote && <Quotation tweet={quote} />}
+          {retweet && <div className="retweeter">Retweeted by {retweet.user.screen_name}</div>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }, [source.id_str, unread]);
+
+  return memo;
 };
 export default Tweet;
 
--- src/renderer/components/TweetList.tsx
+++ src/renderer/components/TweetList.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from "react";
 import {css} from "@emotion/react";
 import Tweet from "./Tweet";
 import {ErrorBoundary} from "react-error-boundary";
@@ -14,39 +15,43 @@ const {facade} = window;
 const TweetList = (props: OwnProps) => {
   const {tweets, lastReadID} = props;
 
-  const elements = tweets.map((tweet) => {
-    const unread = lastReadID != null && tweet.id_str > lastReadID;
+  const memo = useMemo(() => {
+    const elements = tweets.map((tweet) => {
+      const unread = lastReadID != null && tweet.id_str > lastReadID;
 
-    const errorHandler = (error: Error, information: {componentStack: string}) => {
-      facade.logger.error(error.stack);
-      facade.logger.error(tweet);
-    };
+      const errorHandler = (error: Error, information: {componentStack: string}) => {
+        facade.logger.error(error.stack);
+        facade.logger.error(tweet);
+      };
+
+      return (
+        <CSSTransition key={tweet.id_str} timeout={500} classNames="fade">
+          <li data-id={tweet.id_str} className={unread ? "unread" : undefined}>
+            <ErrorBoundary
+              onError={errorHandler}
+              fallbackRender={({error, resetErrorBoundary}) => (
+                <div>
+                  {tweet.id_str}: {error.message}
+                </div>
+              )}
+            >
+              <Tweet source={tweet} unread={unread} />
+            </ErrorBoundary>
+          </li>
+        </CSSTransition>
+      );
+    });
 
     return (
-      <CSSTransition key={tweet.id_str} timeout={500} classNames="fade">
-        <li data-id={tweet.id_str} className={unread ? "unread" : undefined}>
-          <ErrorBoundary
-            onError={errorHandler}
-            fallbackRender={({error, resetErrorBoundary}) => (
-              <div>
-                {tweet.id_str}: {error.message}
-              </div>
-            )}
-          >
-            <Tweet source={tweet} unread={unread} />
-          </ErrorBoundary>
-        </li>
-      </CSSTransition>
+      <div css={styles}>
+        <ol className="tweets">
+          <TransitionGroup exit={false}>{elements}</TransitionGroup>
+        </ol>
+      </div>
     );
-  });
+  }, [tweets.reduce((previous, current) => previous + current.id_str, ""), lastReadID]);
 
-  return (
-    <div css={styles}>
-      <ol className="tweets">
-        <TransitionGroup exit={false}>{elements}</TransitionGroup>
-      </ol>
-    </div>
-  );
+  return memo;
 };
 export default TweetList;
 
